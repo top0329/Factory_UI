35702f1b88d93788b4079aa5aff2d32fb41e57a1
top0329 <top000329@gmail.com>
2024-02-21 07:26:05 +0800

    wip: Implement resposive UI for DefaultBlueprintCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import robot from './assets/svg/robot.svg';
 import { DefaultBlueprintCard } from './components/Cards/default';
+// import { MyBlueprintCard } from './components/Cards/mycard';
 function App() {
   return (
     <div className="flex flex-col gap-2">
@@ -11,6 +12,15 @@ function App() {
         mintPrice={0.001}
         mintLimit={100}
       />
+
+      {/* <MyBlueprintCard
+        imageLink={robot}
+        name="Robot"
+        blueprintid={28}
+        tsupply={1000000000}
+        mintPrice={0.001}
+        mintLimit={100}
+      /> */}
     </div>
   );
 }
--- src/components/Cards/mycard.tsx
+++ src/components/Cards/mycard.tsx
@@ -9,36 +9,37 @@ export interface Props {
   mintLimit: number;
 }
 
-export function DefaultBlueprintCard(props: Props) {
+export function MyBlueprintCard(props: Props) {
   return (
     <div
       id="container"
-      className="w-[176px] sm:w-[280px]  border border-black bg-[#011018] rounded-3xl border-block"
+      className="w-[280px] h-[480px] border border-black bg-[#011018] rounded-3xl border-block"
       style={{ overflow: 'hidden' }}
     >
-      <div className="relative w-[176px] sm:w-[280px]">
+      <div className="relative w-full">
         <div
           id="badge"
-          className="absolute left-[52px] sm:left-[152px] top-[20px] sm:top-[26px] w-[175.5px] h-[20px] sm:h-[30px] bg-[#0047FF] text-white text-center text-[14px] sm:text-[18px] rotate-[38.86deg] py-auto px-[35px] shadow-[0_3px_5px_1px_rgba(0,0,0,0.3)]"
+          className="absolute left-[152px] top-[26px] w-[175.5px] h-[30px] bg-[#0047FF] text-white text-center text-[18px] rotate-[38.86deg] py-auto px-[35px] shadow-[0_3px_5px_1px_rgba(0,0,0,0.3)]"
         >
           Blueprint
         </div>
-        <img
-          src={props.imageLink}
-          className="w-[280px] xs:w-[176px]"
-          alt="okoko"
-        />
+        <img src={props.imageLink} alt="okoko" />
         <div
           id="gradient"
-          className="absolute top-[258px] bg-gradient-to-t from-[#011018] bg-opacity-100 to-[#000407]/0 w-[280px]  xs:w-[176px] h-[82px]"
+          className="absolute top-[258px] bg-gradient-to-t from-[#011018] bg-opacity-100 to-[#000407]/0 w-[280px] h-[82px] mt-"
         ></div>
         <div
           id="infor"
-          className="relative flex justify-between sm:flex-col gap-y-1 top-[-8px] px-6 sm:w-[280px] w-[176px] box-border"
+          className="relative flex flex-col w-[237px] gap-y-1 top-[-8px] left-[25px]"
         >
           <div id="name" className="text-white">
             <p className="text-xs font-mono text-[#858584]">Name</p>
-            <p className="text-lg font-mono">{props.name}</p>
+            <div className="flex justify-between py-[3px]">
+              <p className="text-lg font-mono">{props.name}</p>
+              <p className="flex font-mono items-center rounded-2xl bg-[#06DCEC]/20 text-[11px] my-[3px] px-[6px] border border-[#06DCEC]/50 text-[#06DCEC]/50 text-center">
+                My Blueprint
+              </p>
+            </div>
           </div>
 
           <div id="id_supply" className="flex justify-between text-white">
@@ -46,14 +47,14 @@ export function DefaultBlueprintCard(props: Props) {
               <p className="text-xs font-mono text-[#858584]">ID</p>
               <p className="text-lg font-mono">{props.blueprintid}</p>
             </div>
-            <div id="id" className="text-end hidden sm:block">
+            <div id="id" className="text-end">
               <p className="text-xs font-mono text-[#858584]">Total Supply</p>
               <p className="text-lg font-mono">{props.tsupply}</p>
             </div>
           </div>
 
           <div id="id_supply" className="flex justify-between text-white">
-            <div id="id" className=" hidden sm:block">
+            <div id="id" className="">
               <p className="text-xs font-mono text-[#858584]">Mint Price</p>
               <p className="flex gap-2 text-lg font-mono">
                 <span className="items-center my-auto">
@@ -62,7 +63,7 @@ export function DefaultBlueprintCard(props: Props) {
                 {props.mintPrice} <span className="text-[#F3AC19]">ETH</span>
               </p>
             </div>
-            <div id="id" className="text-end mb-4  hidden sm:block">
+            <div id="id" className="text-end">
               <p className="text-xs font-mono text-[#858584]">Mint Limit</p>
               <p className="text-lg font-mono">{props.mintLimit}</p>
             </div>
