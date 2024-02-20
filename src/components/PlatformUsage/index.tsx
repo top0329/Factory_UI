6dece663ec2f10b5c983c8cb868394963a5597cd
top0329 <top000329@gmail.com>
2024-02-20 16:18:36 +1000

    wip: PlatformUsage web done;

--- src/components/PlatformUsage/index.tsx
+++ src/components/PlatformUsage/index.tsx
@@ -10,14 +10,33 @@ const PlatformUsage: FC = () => {
         <span className="text-white">NFT</span> people
       </h1>
       <div className="grid grid-cols-2 border border-light-gray rounded-3xl shadow-lg shadow-secondary">
-        <div className="col-span-1">
-          <div className="">
-            <hr className="border border- h-full my-auto" />
+        <div className="col-span-1 py-2">
+          <div className="flex p-8">
+            <hr className="border border-cyan h-16 w-0 my-auto ml-4 block" />
+            <p className="pl-10 text-white text-3xl">
+              Enhancing Asset Value Stability and Reusablility using NFT&nbsp;
+              <span className="text-cyan">Synthesis</span>
+            </p>
+          </div>
+          <div className="flex p-8">
+            <hr className="border border-yellow h-16 w-0 my-auto ml-4 block" />
+            <p className="pl-10 text-white text-3xl">
+              Gamified Advertising Platform by creating&nbsp;
+              <span className="text-yellow">Unique</span>&nbsp;NFTs for gaming
+              aseets
+            </p>
+          </div>
+          <div className="flex p-8">
+            <hr className="border border-cyan h-16 w-0 my-auto ml-4 block" />
+            <p className="pl-10 text-white text-3xl">
+              Enhancing Asset Value Stability and Reusablility using NFT&nbsp;
+              <span className="text-cyan">Synthesis</span>
+            </p>
           </div>
         </div>
-        <div className="col-span-1">
+        <div className="relative col-span-1 w-full h-full">
           <img
-            className="rounded-r-3xl"
+            className="w-full h-full rounded-r-3xl object-cover"
             src={ImageForPlatformUsage}
             alt="image-for-platform-usage"
           />
