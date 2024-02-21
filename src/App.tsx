8e4522b7a1f34aa347da3c76b54d659d815b059b
top0329 <top000329@gmail.com>
2024-02-21 13:50:05 +0800

    feat: Add my own blueprint card with responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import robot from './assets/svg/robot.svg';
 import { DefaultBlueprintCard } from './components/Cards/default';
 import { MyBlueprintCard } from './components/Cards/mycard';
+import { MyOwnBlueprintCard } from './components/Cards/myown';
 import { OwnBlueprintCard } from './components/Cards/own';
 import { UserBlueprintCard } from './components/Cards/user';
 function App() {
   return (
     <div className="flex flex-col gap-2">
+      <MyOwnBlueprintCard
+        imageLink={robot}
+        name="Robot"
+        blueprintid={28}
+        tsupply={1000000000}
+        address="0x55d398326f99059ff775485246999027b3197955"
+      />
+
       <OwnBlueprintCard
         imageLink={robot}
         name="Robot"
