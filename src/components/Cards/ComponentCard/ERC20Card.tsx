f25ad062a4335c9350529f54d817173b2871aa74
top0329 <top000329@gmail.com>
2024-02-22 16:21:55 +1000

    wip: ComponentCard and BlueprintDetailDrawer done

--- src/components/Cards/ComponentCard/ERC20Card.tsx
+++ src/components/Cards/ComponentCard/ERC20Card.tsx
@@ -1,14 +1,45 @@
-import { Icon } from '@iconify/react/dist/iconify.js';
 import { FC } from 'react';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
-const ERC20Card: FC = () => {
+export interface Props {
+  imageUrl: string;
+  icon?: boolean;
+}
+
+const ERC20Card: FC<Props> = ({ imageUrl, icon = false }) => {
   return (
-    <button className="flex justify-center items-center bg-secondary w-72 h-44 rounded-3xl">
-      <Icon
-        className="w-24 h-24 text-[#898989]"
-        icon="flowbite:circle-plus-outline"
+    <div className="relative w-[272px] h-44">
+      <img
+        className="flex justify-center items-center w-72 h-44 rounded-3xl"
+        src={imageUrl}
+        alt="erc-20"
       />
-    </button>
+      <p className="absolute top-3 left-4 bg-blue-200 text-blue-800 text-base font-medium me-2 px-2.5 py-0.5 rounded opacity-90">
+        ERC20
+      </p>
+      {icon && (
+        <div>
+          <Icon
+            className="absolute top-3 right-12 bg-blue-200 w-7 h-7 text-blue-800 text-base font-medium me-2 p-0.5 rounded opacity-90 cursor-pointer"
+            icon="mynaui:edit-one"
+          />
+          <Icon
+            className="absolute top-3 right-4 bg-blue-200 w-7 h-7 text-blue-800 text-base font-medium me-2 p-0.5 rounded opacity-90 cursor-pointer"
+            icon="heroicons:trash"
+          />
+        </div>
+      )}
+      <p className="absolute top-28 left-4 bg-blue-200 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded opacity-90">
+        Copper
+      </p>
+      <p className="absolute top-36 left-4 bg-blue-200 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded opacity-90 tracking-tighter">
+        Amount <span className="">100000</span>
+      </p>
+      <p className="absolute top-36 right-2 truncate bg-blue-200 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded opacity-90 tracking-tighter">
+        Address <span className="">0x5868...4be1</span>
+      </p>
+      <div className="absolute top-[118px] rounded-3xl bg-gradient-to-t from-landing via-transparent to-transparent w-full h-1/3"></div>
+    </div>
   );
 };
 
