7c79cad23e4b6654a9cb0f7b1e89bded8bbfd537
top0329 <top000329@gmail.com>
2024-03-06 01:03:15 +1000

    fix: update OwnBlueprint and Product drawer

--- src/components/Drawers/ProductDetailsDrawer.tsx
+++ src/components/Drawers/ProductDetailsDrawer.tsx
@@ -3,17 +3,7 @@ import { Link } from 'react-router-dom';
 import { Icon } from '@iconify/react/dist/iconify.js';
 
 import Button from '../Button';
-// import ERC20Card from '../Cards/ComponentCard/ERC20Card';
-// import ERC721Card from '../Cards/ComponentCard/ERC721Card';
-// import ERC1155Card from '../Cards/ComponentCard/ERC1155Card';
 import { WindowSize } from '../../types';
-
-// import Wood from '../../assets/images/development/wood-erc20.webp';
-// import Copper from '../../assets/images/development/copper-erc20.webp';
-// import Iron from '../../assets/images/development/iron-erc20.webp';
-// import Key from '../../assets/images/development/key-erc721.webp';
-// import Axe from '../../assets/images/development/axe-iron-wood-erc1155.webp';
-import Door from '../../assets/images/development/door-erc1155.png';
 
 export interface Props {
   isDrawerOpen?: boolean;
@@ -94,11 +84,11 @@ const ProductDetailsDrawer: FC<Props> = ({ isDrawerOpen, setIsDrawerOpen }) => {
           </div>
           <img
             className="min-h-[235px] object-cover md:min-h-[435px]"
-            src={Door}
+            src="https://indigo-payable-walrus-596.mypinata.cloud/ipfs/QmZHBY1MB1AzZttMc1WkPiUM68ZqjUkBxxv87znCmfkHQY/iron%20sword.webp"
             alt="drawer"
           />
           <p className="z-30 absolute top-[192px] left-4 text-white text-2xl font-semibold me-2 px-2.5 py-0.5 rounded opacity-90 md:top-[392px]">
-            Iron Sheild
+            Iron Sword
           </p>
           <div className="z-10 absolute top-[124px] bg-gradient-to-t from-landing via-transparent to-transparent w-full h-28 md:top-[324px]"></div>
           <div className="bg-[#011018] py-6 px-8 h-80 md:h-h-[172px]">
