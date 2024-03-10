28769fec395c7f7accec7dc08c39a0f121aa8765
top0329 <top000329@gmail.com>
2024-03-11 08:51:12 +0900

    feat: decompose page complete

--- src/components/Cards/BlueprintCard/ProductCard.tsx
+++ src/components/Cards/BlueprintCard/ProductCard.tsx
@@ -1,92 +1,147 @@
-import { FC } from "react";
-
-import { Icon } from "@iconify/react/dist/iconify.js";
-
+import { FC, useState } from 'react';
+import { Icon } from '@iconify/react/dist/iconify.js';
+import Button from '../../Button';
 export interface Props {
   uri: string;
   name: string;
   blueprintId: number;
-  totalSupply: number;
+  balance: number;
   address: string;
+  myBlueprint?: boolean;
   onClick?: () => void;
 }
 
-const BlueprintCard: FC<Props> = ({
+const ProductCard: FC<Props> = ({
   uri,
   name,
   blueprintId,
-  totalSupply,
+  balance,
   address,
+  myBlueprint,
   onClick,
 }) => {
+  const [tooltipMessage, setTooltipMessage] = useState('Copy to clipboard');
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(address).then(() => {
+      setTooltipMessage('Copied!'); // Update tooltip message on success
+      setTimeout(() => {
+        setTooltipMessage('Copy to clipboard'); // Reset tooltip message after delay
+      }, 2000); // Duration before resetting the tooltip message
+    });
+  };
   return (
     <div
       id="container"
-      className="w-[176px] sm:w-full border border-black bg-[#011018] rounded-3xl border-block overflow-clip"
+      className=" h-min xs:w-full sm:w-full md:w-full border-2 border-[#00F0FF]/30 bg-[#011018] rounded-3xl border-block overflow-clip"
       onClick={onClick}
     >
-      <div className="relative w-[176px] sm:w-full overflow-hidden">
+      <div className="relative xs:w-full sm:w-full md:w-full overflow-hidden">
         <div
           id="badge"
-          className="absolute right-[-38px] sm:right-[-38px] top-[20px] sm:top-[26px] w-[175.5px] h-[20px] sm:h-[30px] bg-[#0047FF] text-white text-center text-[14px] sm:text-[18px] rotate-[38.86deg] py-auto px-[35px] shadow-[0_3px_5px_1px_rgba(0,0,0,0.3)]"
+          className="absolute  right-[-55px] md:right-[-48px] top-[11px] md:top-[18px] sm:top-[15px] sm:right-[-55px] xs:h-[22px] xs:top-[12px] xs:right-[-60px] xs:text-[12px] w-[175.5px] h-[25px] md:h-[27px] bg-[#FFF500] text-black text-center text-[14px] md:text-[18px] rotate-[38.86deg] py-auto px-[35px] shadow-[0_3px_5px_1px_rgba(0,0,0,0.3)]"
         >
           Product
         </div>
-        <div className="w-[176px] h-[176px] sm:w-full sm:h-full overflow-hidden object-cover">
+        <div className="xs:w-full sm:w-full md:w-full md:h-full overflow-hidden object-cover">
           <img
             src={uri}
-            className="w-full sm:w-full md:w-full lg:w-full aspect-auto object-cover"
+            className="w-full xs:w-full sm:w-full lg:w-full aspect-auto object-cover"
             alt="okoko"
           />
+          <div className="absolute md:bottom-[202px] lg:bottom-[214px] sm:bottom-[190px] bg-gradient-to-t from-[#011018] from-0% sm:from-0% bg-opacity-100 to-[#000407]/0 bottom-[106px] w-full h-[45px]"></div>
         </div>
-        <div
-          id="gradient"
-          className="relative top-[-45px] bg-gradient-to-t from-[#011018] bg-opacity-100 to-[#000407]/0 sm:w-full h-[82px]"
-        ></div>
+        <p
+          className={`absolute ${
+            myBlueprint ? 'sm:hidden' : 'hidden'
+          } flex bottom-[126px] right-[10px] block-content font-mono items-center rounded-2xl bg-[#06DCEC]/20 text-[11px] px-[6px] border border-[#06DCEC]/50 text-[#06DCEC] text-center`}
+        >
+          My Blueprint
+        </p>
         <div
           id="infor"
-          className="relative flex justify-between sm:flex-col gap-y-1 top-[-80px] sm:top-[-80px] px-6 w-[176px] sm:w-full  box-border"
+          className="flex justify-between p-4 pt-0 sm:flex-col xs:px-4 gap-y-0 top-[-80px] md:top-[-80px] w-full box-border"
         >
           <div id="name" className="text-white">
-            <p className="text-xs font-mono text-[#858584]">Name</p>
-            <p className="text-lg font-mono">{name}</p>
+            <p className="flex justify-start text-xs font-mono text-[#858584]">
+              Name
+            </p>
+            <div className="sm:flex sm:justify-between grid items-center py-[3px]">
+              <p className="w-auto truncate text-sm md:text-base lg:text-lg font-mono">
+                {name}
+              </p>
+              <p
+                className={`${
+                  myBlueprint ? 'hidden sm:block' : 'hidden'
+                } truncate font-mono items-center rounded-2xl bg-[#06DCEC]/20 text-[11px] px-[6px] border border-[#06DCEC]/50 text-[#06DCEC] text-center w-[87px]`}
+              >
+                My Blueprint
+              </p>
+            </div>
           </div>
-
           <div id="id_supply" className="flex justify-between text-white">
-            <div id="id" className="">
-              <p className="text-xs font-mono text-[#858584]">ID</p>
-              <p className="text-lg font-mono">{blueprintId}</p>
+            <div id="id" className="pl-1">
+              <p className="flex justify-start text-xs font-mono text-[#858584]">
+                ID
+              </p>
+              <p className="text-sm md:text-base lg:text-lg items-center font-mono mt-[3.2px] ">
+                {blueprintId}
+              </p>
             </div>
             <div id="id" className="text-end hidden sm:block">
               <p className="text-xs font-mono text-[#858584]">Balance</p>
-              <p className="text-lg font-mono">{totalSupply}</p>
+              <p className="md:text-base lg:text-lg font-mono mt-[4px]">
+                {balance}
+              </p>
             </div>
           </div>
 
-          <div id="id_supply" className="flex justify-between text-white">
+          <div id="id_supply" className="relative w-full hidden sm:block">
             <div id="address" className="text-white">
-              <p className="text-xs font-mono text-[#858584]">Address</p>
+              <p className="text-xs font-mono text-[#858584]">Creator</p>
               <div id="id_supply" className="flex justify-between text-white">
-                <div className="flex justify-center gap-1 item-center sm:text-lg font-mono text-xs">
+                {/* Other content */}
+                <div className="flex justify-center gap-1 item-center md:text-base lg:text-lg font-mono text-xs">
                   <Icon
                     icon="logos:ethereum"
-                    className="hidden sm:block item-center my-auto"
+                    className="hidden md:block item-center my-auto"
                   />
-                  {address.substring(0, 7)} ... {address.slice(-5)}
+                  <p>
+                    {address.substring(0, 7)}...{address.slice(-5)}
+                  </p>
                 </div>
-                <button>
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    if (copyToClipboard) {
+                      copyToClipboard();
+                    }
+                  }}
+                >
                   <Icon
                     icon="solar:copy-outline"
                     className="item-center my-auto"
                   />
                 </button>
+                <div
+                  role="tooltip"
+                  className={`absolute z-10 inline-block right-0 bottom-7 px-3 py-2 text-sm text-white transition-opacity duration-300 bg-gray-700 rounded-lg shadow-sm ${
+                    tooltipMessage === 'Copied!'
+                      ? 'opacity-100'
+                      : 'invisible opacity-0'
+                  }`}
+                >
+                  {tooltipMessage}
+                  <div className="tooltip-arrow" data-popper-arrow></div>
+                </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className='p-4 pt-0'>
+          <Button text="Decompose" className='rounded-lg w-full justify-center xs:h-10 h-8 xs:text-[16px] text-[14px]'></Button>
         </div>
       </div>
     </div>
   );
 };
-
-export default BlueprintCard;
+export default ProductCard;
