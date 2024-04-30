d8765c663b4c25980b144999995550ea103a1636
top0329 <top000329@gmail.com>
2024-04-30 15:54:40 +0800

    feat: add lazy loading property and preload function to 404 page

--- src/pages/Maintenance/PageNotFound.tsx
+++ src/pages/Maintenance/PageNotFound.tsx
@@ -1,9 +1,16 @@
+import { HeadProvider } from 'react-head';
 import Image404 from '../../assets/images/bg_404_text.png';
+import { Helmet } from 'react-helmet';
 
 export default function PageNotFoundPage() {
   return (
-    <div className="min-w-[320px] bg-contain bg-no-repeat bg-center">
-      <img src={Image404} alt="404 image"></img>
-    </div>
+    <HeadProvider>
+      <Helmet>
+        <link rel="preload" as="image" href={Image404} />
+      </Helmet>
+      <div className="min-w-[320px] bg-contain bg-no-repeat bg-center">
+        <img src={Image404} alt="404 image"></img>
+      </div>
+    </HeadProvider>
   );
 }
